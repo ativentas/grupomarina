8bf44aae03f2abcf7a6a38f9ca316c275c00ab1e
jose <ativentas@hotmail.com>
2016-12-16 13:14:21 +0100

    configuracion

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,10 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
 
-        Model::unguard();	
-		$this->call(EventsTableSeeder::class);
-        Model::reguard();
+  //       Model::unguard();	
+		// $this->call(EventsTableSeeder::class);
+  //       Model::reguard();
     }
 }
